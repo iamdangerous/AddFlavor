e12a5141995c734195ad6505c06802661f0ffe7a
lohra <rahul@hatchitup.com>
2016-10-04 14:40:09 +0530

    ss

---
+++ app/src/freeDebug/java/com/udacity/gradle/flavorspecificactivity/free/FreeJokeActivity.java
@@ -0,0 +1,15 @@
+package com.udacity.gradle.flavorspecificactivity.free;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.udacity.gradle.flavorspecificactivity.R;
+
+public class FreeJokeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_free_joke);
+    }
+}
